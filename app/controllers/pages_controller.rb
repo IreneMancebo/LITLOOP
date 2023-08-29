5808166c2238af24d1204b5a33235eb321370fa0
Irene Mancebo <irenemancebo18@gmail.com>
2023-08-29 17:45:47 +0200

    Edit users controller and remove profiles controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,11 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
-  def home
-  end
-
   def feed
 
   end
 
   def dashboard
-
-  end
-
-  def show
 
   end
 end
