26c3e104b1cc7fa6490d92576aca915531606317
Nicole Chae <elementalmorning@gmail.com>
2023-08-31 16:55:09 +0200

    import swiper for image carousel capabilities // alternative colorway used, not an executive change // lil post-it note for location description // added access to username via nook controller // created flash page for directory with some text as placeholder, rendered on application.html.erb // last-minute attempt at a decorative container for footnote in a postcard, bit cheesy, will change tomorrow

---
+++ app/javascript/controllers/swiper_controller.js
@@ -0,0 +1,33 @@
+import { Controller } from "@hotwired/stimulus"
+import Swiper from "swiper"
+
+// Connects to data-controller="swiper"
+export default class extends Controller {
+  connect() {
+
+    const swiper = new Swiper('.swiper', {
+      direction: 'horizontal',
+      slidesPerView: 1,
+      spaceBetween: 30,
+      effect: "fade",
+      loop: true,
+
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        hide: true,
+      },
+
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
+    console.log(swiper);
+  };
+};
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+pin "swiper", to: "https://ga.jspm.io/npm:swiper@10.2.0/swiper.mjs"
