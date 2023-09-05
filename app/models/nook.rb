30fa3394990c3deb6f3395d09932ead6f6e563f6
Nicole Chae <elementalmorning@gmail.com>
2023-09-05 17:48:23 +0200

    it's the little things

--- app/models/nook.rb
+++ app/models/nook.rb
@@ -3,4 +3,8 @@ class Nook < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_coordinates?
   has_many_attached :photos
   has_many :footnotes
+
+  def coordinates
+    "#{longitude}, #{latitude}"
+  end
 end
