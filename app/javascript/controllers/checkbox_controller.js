5e87de32eb91765b919252c311206329ac58dc27
Brett Hodge <brettchodge@gmail.com>
2023-09-06 17:26:38 +0200

    animate accordion open

--- app/javascript/controllers/checkbox_controller.js
+++ app/javascript/controllers/checkbox_controller.js
@@ -21,10 +21,10 @@ for (i = 0; i < acc.length; i++) {
 
     /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
     } else {
-      panel.style.display = "block";
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
 }
